0685de71a8f611228717960bf4eedcdf15459155
Mehdi Brillaud <mehdibrillaud@mbp-mehdi.home>
2024-05-07 14:07:40 +0200

    Ajoute les différentes routes front-end

--- routes/front-end.js
+++ routes/front-end.js
@@ -3,7 +3,6 @@ const auth = require('../middlewares/auth');
 const router = express.Router();
 
 router.get('/bo', auth, function(req, res) {
-    console.log('res : ', req.auth);
     if(req.auth.userStatus === "admin") {
         res.render('./bo.njk');
     } else {
@@ -12,6 +11,21 @@ router.get('/bo', auth, function(req, res) {
 });
 router.get('/login', (req, res) => {
     res.render('./login.njk');
-})
+});
+router.get('/signup', (req, res) => {
+    res.render('./signup.njk');
+});
+router.get('/', (req, res) => {
+    res.render('./index.njk');
+});
+router.get('/news', (req, res) => {
+    res.render('./news.njk');
+});
+router.get('/appointment', (req, res) => {
+    res.render('./appointment.njk');
+});
+router.get('/services', (req, res) => {
+    res.render('./services.njk');
+});
 
 module.exports = router;
