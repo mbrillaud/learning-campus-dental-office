466d456760e26ddf9a379d4da74b732dd7501e7d
Mehdi Brillaud <mehdibrillaud@mbp-mehdi.lan>
2024-05-10 12:53:02 +0200

    Affiche les horaires d'ouvertures sur la page d'accueil

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,13 @@
 const Service = require('../models/Service');
+const Schedules = require('../models/Schedules');
 
 exports.renderIndex = async (req, res) => {
     try {
         const services = await Service.findAll();
+        const schedules = await Schedules.findAll();
         res.render('index.njk', {
             services: services,
+            schedules: schedules,
             isAuthenticated: res.locals.isAuthenticated,
             isAdmin: res.locals.isAdmin
         });
