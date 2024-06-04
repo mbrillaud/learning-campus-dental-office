f0ef1b6e816905c563eef5456f78089b09a7b977
Mehdi Brillaud <mehdibrillaud@mbp-mehdi.lan>
2024-06-04 09:33:22 +0200

    Refactorises les méthodes asynchrones restantes avec la syntaxe async

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,3 +1,10 @@
+/**
+ * Middleware pour vérifier si l'utilisateur est un administrateur.
+ * @param {Object} req - L'objet de requête.
+ * @param {Object} res - L'objet de réponse.
+ * @param {Function} next - La fonction du prochain middleware.
+ * @returns {void}
+ */
 module.exports = (req, res, next) => {
     if (req.auth.userRole === 'admin') {
         return next();
