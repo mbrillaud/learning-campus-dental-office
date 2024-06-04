6f9509e6fd3c014816d62c9a7c60ed9621421901
Mehdi Brillaud <mehdibrillaud@mbp-mehdi.lan>
2024-06-04 10:09:14 +0200

    Ajoute le middleware auth sur la route pour modifier un user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const usersCtrl = require('../controllers/users');
+const auth = require('../middlewares/auth');
 const isAdmin = require('../middlewares/isAdmin');
 
 const router = express.Router();
@@ -140,6 +141,6 @@ const router = express.Router();
 
 router.post('/signup', usersCtrl.signup);
 router.post('/login', usersCtrl.login);
-router.put('/:id', isAdmin, usersCtrl.updateUser);
+router.put('/:id', auth, isAdmin, usersCtrl.updateUser);
 
 module.exports = router;
